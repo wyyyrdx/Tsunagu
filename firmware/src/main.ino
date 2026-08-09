4e229c0acef9cc9fb80f55fa485c8335328af42a
waaddaraghmeh0-sudo <waaddaraghmeh0@gmail.com>
2026-08-09 06:07:42 +0300

    Tsungu main

--- firmware/src/main.ino
+++ firmware/src/main.ino
@@ -1,8 +1,16 @@
-int thumb, index, middle, ring, pinky;
+#define THUMB_PIN 1
+#define INDEX_PIN 2
+#define MIDDLE_PIN 3
+#define RING_PIN 4
+#define PINKY_PIN 5
 
+
+
+int thumb, indexfinger, middle, ring, pinky;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(1000);
 }
 
 void loop() {
@@ -13,24 +21,25 @@ void loop() {
 }
 
 void readsensors() {
-  int thumbRaw = analogRead(A0);
-  int indexRaw = analogRead(A1);
-  int middleRaw = analogRead(A2);
-  int ringRaw = analogRead(A3);
-  int pinkyRaw = analogRead(A4);
+  int thumbRaw = analogRead(THUMB_PIN);
+  int indexRaw = analogRead(INDEX_PIN);
+  int middleRaw = analogRead(MIDDLE_PIN);
+  int ringRaw = analogRead(RING_PIN);
+  int pinkyRaw = analogRead(PINKY_PIN);
 
   thumb = constrain(map(thumbRaw, 256, 59, 0, 100), 0, 100);
-  index = constrain(map(indexRaw, 256, 59, 0, 100), 0, 100);
+  indexfinger = constrain(map(indexRaw, 256, 59, 0, 100), 0, 100);
   middle = constrain(map(middleRaw, 256, 59, 0, 100), 0, 100);
   ring = constrain(map(ringRaw, 256, 59, 0, 100), 0, 100);
   pinky = constrain(map(pinkyRaw, 256, 59, 0, 100), 0, 100);
 }
 
+
 void printvalues() {
   Serial.print("T:");
   Serial.print(thumb);
   Serial.print(" I:");
-  Serial.print(index);
+  Serial.print(indexfinger);
   Serial.print(" M:");
   Serial.print(middle);
   Serial.print(" R:");
@@ -42,13 +51,13 @@ void printvalues() {
 
 
 void detectletter() {
-  if (thumb > 60 && index > 60 && middle > 60 && ring > 60 && pinky > 60) {
+  if (thumb > 60 && indexfinger > 60 && middle > 60 && ring > 60 && pinky > 60) {
     Serial.println("Letter: A");
   }
-  else if (thumb > 60 && index < 40 && middle < 40 && ring < 40 && pinky < 40) {
+  else if (thumb > 60 && indexfinger < 40 && middle < 40 && ring < 40 && pinky < 40) {
     Serial.println("Letter: B");
   }
-  else if (thumb > 40 && thumb < 80 && index > 40 && index < 80 &&
+  else if (thumb > 40 && thumb < 80 && indexfinger > 40 && indexfinger < 80 &&
            middle > 40 && middle < 80 && ring > 40 && ring < 80 &&
            pinky > 40 && pinky < 80) {
     Serial.println("Letter: C");
